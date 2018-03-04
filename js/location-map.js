90f5a8e027153cd20f11270e51221bf7f07e46ca
dpsnow <deep.snow@ya.ru>
2018-03-04 22:28:55 +0100

    Разметка главной и каталога. Предварительная разметка блоков с формой.

---
+++ js/location-map.js
@@ -0,0 +1,22 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+        center: [59.939304, 30.329127],
+        zoom: 16,
+        controls: ['zoomControl']
+    }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        
+        myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
+        }, {
+                iconLayout: 'default#image',
+                iconImageHref: 'img/map-pin.png',
+                iconImageSize: [80, 140],
+                iconImageOffset: [-40, -140]
+            });
+    myMap.behaviors
+        // Отключаем изменение масштаба колесом мыши
+        .disable(['scrollZoom']);
+    myMap.geoObjects
+        .add(myPlacemark);
+});
