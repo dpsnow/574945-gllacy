f6e8765d179592401c43e63f12549af4db87f826
dpsnow <deep.snow@ya.ru>
2018-03-16 20:47:34 +0100

    Тень для маркера на карте

--- js/location-map.js
+++ js/location-map.js
@@ -3,20 +3,43 @@ ymaps.ready(function () {
         center: [59.939304, 30.329127],
         zoom: 16,
         controls: ['zoomControl']
-    }, {
-            searchControlProvider: 'yandex#search'
-        }),
-        
-        myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
-        }, {
-                iconLayout: 'default#image',
-                iconImageHref: 'img/map-pin.svg',
-                iconImageSize: [80, 140],
-                iconImageOffset: [-40, -140]
-            });
+      }, {
+        searchControlProvider: 'yandex#search'
+      }),
+  
+      myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
+      }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/map-pin.svg',
+        iconImageSize: [80, 140],
+        iconImageOffset: [-40, -140]
+        // Определим интерактивную область над картинкой.
+        // iconShape: {
+        //   type: 'Polygon',
+        //   coordinates: [
+        //   ],
+        // },
+        // Тень
+        // iconShadowLayout: 'default#image',
+        // iconShadowHref: 'img/map-pin-shadow.png',
+        // iconShadowImageSize: [182, 110],
+        // iconShadowImageOffset: [0, -110]
+      }),
+      // Тень как еще одна метка - переделать
+      myPlacemarkShadow = new ymaps.Placemark([59.938631, 30.323055], {
+      }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/map-pin-shadow.png',
+        iconImageSize: [182, 110],
+        iconImageOffset: [0, -110],
+        cursor: 'default'
+      });
+      
     myMap.behaviors
-        // Отключаем изменение масштаба колесом мыши
-        .disable(['scrollZoom']);
+      // Отключаем изменение масштаба колесом мыши
+      .disable(['scrollZoom']);
     myMap.geoObjects
-        .add(myPlacemark);
-});
+      .add(myPlacemarkShadow);
+    myMap.geoObjects
+      .add(myPlacemark);
+  });
